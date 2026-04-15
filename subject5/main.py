d84c8e30cb9b81513d1ec1d47993c14916c08ab1
DH-Jeon <wjsdonghyeon@dgu.ac.kr>
2026-04-15 16:49:28 +0900

    Add school

--- subject5/main.py
+++ subject5/main.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
 dept_id = input("학과를 입력하세요: ")
+school_name = input("학교 이름을 입력하세요: ")
 
 print()
 print(f"이름: {name}")
 print(f"학번: {student_id}")
 print(f"학과: {dept_id}")
+print(f"학교 이름: {school_name}")
